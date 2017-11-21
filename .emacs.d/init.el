bd9e07963f63b6e81c4df16c76d9926b071e114e
Berto Yáñez <berto@ber.to>
2017-11-21 15:49:08 +0100

    Fix error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,17 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+        (package-refresh-contents)
+        (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 (setq custom-file "~/.emacs.d/custom-settings.el")
 (load custom-file t)
 
+(put 'dired-find-alternate-file 'disabled nil)
